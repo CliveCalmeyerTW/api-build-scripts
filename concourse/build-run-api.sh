966480f29ef92ff8d9308d4dc9bf2e14841454ab
Clive Calmeyer <ccalmeye@thoughtworks.com>
2018-06-15 15:21:37 +0100

    Add GOPATH and app dependencies

--- concourse/build-run-api.sh
+++ concourse/build-run-api.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+export GOPATH=$(pwd)/gopath
+cd gopath/src/github.com/CliveCalmeyerTW/address_book_go
+
+go get -u github.com/go-chi/chi
+go get -u github.com/go-chi/render
+go get -u github.com/lib/pq
+
 go build
 go install
 address_book_go
