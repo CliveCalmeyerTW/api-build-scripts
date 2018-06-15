650e5e1a5536f3c74a46ed9ab8f695484925fa61
Clive Calmeyer <ccalmeye@thoughtworks.com>
2018-06-15 15:37:53 +0100

    build dependencies

--- concourse/build-run-api.sh
+++ concourse/build-run-api.sh
@@ -5,9 +5,9 @@ set -e
 export GOPATH=$(pwd)/gopath
 cd gopath/src/github.com/CliveCalmeyerTW/address_book_go
 
-go get -u github.com/go-chi/chi
-go get -u github.com/go-chi/render
-go get -u github.com/lib/pq
+go build github.com/go-chi/chi
+go build github.com/go-chi/render
+go build github.com/lib/pq
 
 go build
 go install
